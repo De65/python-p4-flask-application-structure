4375e5b35be470392170fe49d816ecf2b8eec911
De65 <kyalodiana6@gmail.com>
2023-06-20 08:49:36 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'Hello, {username}!'
+
+if __name__ == '__main__':
+    app.run(port=5555)
